9201b443355f72c31787fbdd5282a2ba226253a2
hamidrezaHemati <hrhhemati@gmail.com>
2024-07-22 00:09:16 +0330

    nothing

--- simple_av/planning.py
+++ simple_av/planning.py
@@ -102,7 +102,7 @@ class Planning(Node):
 
         self.curve_finish_point = {}
         
-        self.dest_lanelet = "lanelet147"
+        self.dest_lanelet = "lanelet179"
         # dest_lanelet = "lanelet319"
         # dest_lanelet = "lanelet335"
         
